db7a451b733315f549bbb3ed9049dd80bbb0caa0
Nier <stu5405@gmail.com>
2014-09-01 20:28:26 +0800

    新增一堆測試新歌

--- js/search.js
+++ js/search.js
@@ -30,6 +30,32 @@ function search(e) {
     $("#result").html(result_html);
 }
 
+function list_test_songs(e) {
+    var title = $("#title").val();
+    var level_low = +$("#level_low").val();
+    var level_high = +$("#level_high").val();
+
+    var music_test_db = window.opener.music_test_db;
+    var result = music_test_db({ title: { like: title }, level: { ">=": level_low, "<=": level_high } }).order("level asec, type asec, title asec");
+    var result_html = "<p>" + result.count() + " songs found.</p>";
+
+    result.each(function (entry) {
+        var type_str = "";
+        if (entry.type == 1)
+            type_str = "NOV";
+        else if (entry.type == 2)
+            type_str = "ADV";
+        else if (entry.type == 3)
+            type_str = "EXH";
+        else if (entry.type == 4)
+            type_str = "INF";
+
+        result_html += "<a href='javascript:ret_val(\"" + entry.value + "\");'>(" + entry.level + ") " + entry.title + " [" + type_str + "]</a><br/>";
+    });
+
+    $("#result").html(result_html);
+}
+
 function titleKeyUp(e) {
     if (e.keyCode == 13) {
         search();
@@ -38,6 +64,7 @@ function titleKeyUp(e) {
 
 $(document).ready(function() {
     $("#search").on("click", search);
+    $("#test_songs").on("click", list_test_songs);
     $("#title").focus();
     $("#title").on("keyup", titleKeyUp);
 
